5efe77e9f1f5a9023f9121a6b7b2cd3309bf6def
裴建武 <2273313521@qq.com>
2018-11-24 13:58:21 +0800

    '完成城市列表'

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <citySearch></citySearch>
+        <cityList></cityList>
+        <cityAlphabet></cityAlphabet>
     </div>
 </template>
 <script>
 import cityHeader from "./components/header.vue"
 import citySearch from "./components/search.vue"
+import cityList from "./components/list.vue"
+import cityAlphabet from "./components/alphabet.vue"
 
     export default{
         name:"city",
         components:{
             cityHeader,
-            citySearch
+            citySearch,
+            cityList,
+            cityAlphabet
         }
     }
 </script>
