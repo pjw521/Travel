15dd2e243d04e77613c62d660f1c32c5d4ccd253
裴建武 <2273313521@qq.com>
2018-11-26 19:32:52 +0800

    '完成banner图片点击查看功能'

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div class="banner" @click="showGallary=true">
+            <img class="banner-img" src="http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-title">
+                    大连圣亚海洋世界
+                </div>
+                <div class="banner-number" >
+                    <i class="iconfont banner-icon">&#xe616;</i>
+                    39
+                </div>
+            </div>
+        </div>
+        <CommonGallary :imgs="imgs" v-show="showGallary" @change="change"></CommonGallary>
+    </div>
+</template>
+<script>
+import CommonGallary from "@common/gallary/gallary"
+
+    export default{
+        name:"DetailBanner",
+        data(){
+            return{
+                showGallary:false,
+                imgs:["http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg","http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"]
+            }
+        },
+        methods:{
+            change(){
+                this.showGallary = false;
+            }
+        },
+        components:{
+            CommonGallary
+        }
+    }
+</script>
+<style lang="stylus" scoped>
+@font-face {
+  font-family: 'iconfont';  /* project id 928739 */
+  src: url('//at.alicdn.com/t/font_928739_anof4qqt6yc.eot');
+  src: url('//at.alicdn.com/t/font_928739_anof4qqt6yc.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_928739_anof4qqt6yc.woff') format('woff'),
+  url('//at.alicdn.com/t/font_928739_anof4qqt6yc.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_928739_anof4qqt6yc.svg#iconfont') format('svg');
+}
+    .banner
+        position relative
+        overflow hidden
+        height 0
+        padding-bottom 55%
+        .banner-img
+            width 100%
+        .banner-info
+            display flex
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            line-height .6rem
+            color #fff
+            background-image linear-gradient(top,rgba(0,0,0,0,),rgba(0,0,0,0.8))
+            .banner-title
+                flex 1
+                font-size .32rem
+                padding 0 .2rem
+            .banner-number
+                height 0.32rem
+                line-height .32rem
+                margin-top .2rem
+                padding 0 .4rem
+                border-radius .2rem
+                background rgba(0,0,0,0.8)
+                font-size .24rem
+                .banner-icon 
+                 font-size .24rem
+</style>
--- src/pages/home/components/recommend.vue
+++ src/pages/home/components/recommend.vue
@@ -2,7 +2,12 @@
     <div>
         <div class="title">热销推荐</div>
         <ul>
-            <li class="item border-bottom" v-for="item in recommendList" :key="item.id">
+            <router-link 
+                :to="/detail/ + item.id"
+                tag="li"
+                class="item border-bottom" 
+                v-for="item in recommendList" 
+                :key="item.id">
                 <img  class="item-img" :src="item.imgUrl" alt="">
                 <div class="item-info">
                     <p class="item-title">{{item.title}}</p>
@@ -11,7 +16,7 @@
                     </p>
                     <button class="item-button">查看详情</button>
                 </div>
-            </li>
+            </router-link>
         </ul>
     </div>
 </template>
